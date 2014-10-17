a62d629bcf8a95a28f11e6642147a2947459494f
DavidWee <dgwee9@gmail.com>
2014-10-17 13:59:17 -0500

    EVERYTHING

--- src/com/example/abbeyapp/MainActivity.java
+++ src/com/example/abbeyapp/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.abbeyapp;
 
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
-import android.R;
+import android.R.*;
 import android.support.v7.app.ActionBarActivity;
 import android.app.Activity;
 import android.content.res.Configuration;
